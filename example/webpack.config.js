c75e04c3eaaa38428bd928d06129d4b28b4ac08f
Sean Matheson <sean@ctrlplusb.com>
2016-10-14 13:52:52 +0100

    Updated example.

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,26 +1,14 @@
 const path = require('path');
 const webpack = require('webpack');
 
-// :: bool -> (Any, Any) -> Any
-function ifElse(condition) {
-  return (then, or) => (condition ? then : or);
-}
-
-// :: [Any] -> [Any]
-function removeEmpty(x) {
-  return x.filter(y => !!y);
-}
-
 module.exports = function configFactory() {
-  const ifDev = ifElse(process.env.NODE_ENV === 'development');
-
   return {
     entry: {
-      app: removeEmpty([
-        ifDev('react-hot-loader/patch'),
-        ifDev('webpack-hot-middleware/client?reload=true'),
+      app: [
+        'react-hot-loader/patch',
+        'webpack-hot-middleware/client?reload=true',
         path.resolve(__dirname, './src/index.js'),
-      ]),
+      ],
     },
     output: {
       path: path.resolve(__dirname, './build'),
@@ -29,13 +17,13 @@ module.exports = function configFactory() {
       chunkFilename: '[name]-[chunkhash].js',
       publicPath: '/bundles/',
     },
-    plugins: removeEmpty([
+    plugins: [
       new webpack.NoErrorsPlugin(),
-      ifDev(new webpack.HotModuleReplacementPlugin()),
+      new webpack.HotModuleReplacementPlugin(),
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
       }),
-    ]),
+    ],
     module: {
       rules: [
         {
