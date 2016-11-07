0634903d46d0545ea684dfbbbb08050885908f09
Sean Matheson <sean@ctrlplusb.com>
2016-11-07 22:15:45 +0000

    Fixes issue with negative number chunk ids.

--- modules/utils/chunkNameHash.js
+++ modules/utils/chunkNameHash.js
@@ -15,7 +15,7 @@ function chunkNameHash(chunkName: string) {
     hash = ((hash << 5) - hash) + char;
     hash = hash & hash; // Convert to 32bit integer
   }
-  return hash;
+  return Math.abs(hash);
 }
 
 export default chunkNameHash;
